7a316c243a8eda65b360548d95e88022ab4cdbb5
Akhil Rajan <rajanakhil07@gmail.com>
2024-04-20 20:45:52 +0530

    feat: added problems controller, router structure

---
+++ src/controllers/problem.controller.js
@@ -0,0 +1,23 @@
+function pingCheckController(req, res) {
+  console.log("ping check");
+  return res.json({ message: "Problem controller is set up" });
+}
+
+function getProblem(req, res) {}
+
+function getProblems(req, res) {}
+
+function addProblem(req, res) {}
+
+function deleteProblem(req, res) {}
+
+function updateProblem(req, res) {}
+
+module.exports = {
+  getProblem,
+  getProblems,
+  addProblem,
+  deleteProblem,
+  updateProblem,
+  pingCheckController,
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 
 const { PORT } = require("./config/server.config");
+const apiRouter = require("./routes/index");
 
 const app = express();
 
+/* MIDDLEWARES */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.text());
+
+/*  ROUTES MIDDLEWARE*/
+app.use("/api", apiRouter);
 
 app.get("/ping", (req, res) => {
   return res.json({ message: "problem service is alive" });
