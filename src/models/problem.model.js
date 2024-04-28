fc35e5e6f3cd1fe7dd78af836eed82b4f502c37c
Akhil Rajan <rajanakhil07@gmail.com>
2024-04-28 13:54:05 +0530

    added problem schema in models

---
+++ src/models/problem.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const problemSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Title can't be empty"],
+  },
+  description: {
+    type: String,
+    required: [true, "Description can't be empty"],
+  },
+  difficulty: {
+    type: String,
+    enum: ["Easy", "Medium", "Hard"],
+    required: [true, "Difficulty can't be empty"],
+    default: "Easy",
+  },
+  testCases: [
+    {
+      input: {
+        type: String,
+        required: true,
+      },
+      output: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  editorial: {
+    type: String,
+  },
+});
+
+const Problem = mongoose.model("Problem", problemSchema);
+
+export default Problem;
