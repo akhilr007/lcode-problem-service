b70eebbde1b8ee70701099d3ee75c0219cc8f284
Akhil Rajan <rajanakhil07@gmail.com>
2024-04-28 20:42:22 +0530

    feat: completed getAllProblems api

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -17,9 +17,15 @@ function getProblem(req, res, next) {
   }
 }
 
-function getProblems(req, res, next) {
+async function getProblems(req, res, next) {
   try {
-    throw new NotImplementedError("getProblems");
+    const response = await problemService.getAllProblems();
+    return res.status(StatusCodes.OK).json({
+      success: true,
+      message: "Successfully retrieved all the problems",
+      error: {},
+      data: response,
+    });
   } catch (error) {
     next(error);
   }
--- src/models/problem.model.js
+++ src/models/problem.model.js
@@ -1,36 +1,41 @@
 const mongoose = require("mongoose");
 
-const problemSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Title can't be empty"],
-  },
-  description: {
-    type: String,
-    required: [true, "Description can't be empty"],
-  },
-  difficulty: {
-    type: String,
-    enum: ["Easy", "Medium", "Hard"],
-    required: [true, "Difficulty can't be empty"],
-    default: "Easy",
-  },
-  testCases: [
-    {
-      input: {
-        type: String,
-        required: true,
-      },
-      output: {
-        type: String,
-        required: true,
-      },
+const problemSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title can't be empty"],
     },
-  ],
-  editorial: {
-    type: String,
+    description: {
+      type: String,
+      required: [true, "Description can't be empty"],
+    },
+    difficulty: {
+      type: String,
+      enum: ["Easy", "Medium", "Hard"],
+      required: [true, "Difficulty can't be empty"],
+      default: "Easy",
+    },
+    testCases: [
+      {
+        input: {
+          type: String,
+          required: true,
+        },
+        output: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    editorial: {
+      type: String,
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Problem = mongoose.model("Problem", problemSchema);
 
